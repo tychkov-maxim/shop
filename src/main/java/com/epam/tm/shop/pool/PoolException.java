e8ecc11ed42a2f2990017ee954d727b8226aae47
Tychkov Maxim <tichkov_maxim@mail.ru>
2016-11-15 11:47:24 +0200

    add pool

---
+++ src/main/java/com/epam/tm/shop/pool/PoolException.java
@@ -0,0 +1,16 @@
+package com.epam.tm.shop.pool;
+
+public class PoolException extends Exception{
+
+    public PoolException(String message) {
+        super(message);
+    }
+
+    public PoolException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PoolException(Throwable cause) {
+        super(cause);
+    }
+}
