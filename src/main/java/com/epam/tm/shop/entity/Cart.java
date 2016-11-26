b904fdacbd903726185b523aadd83601821257f6
Tychkov Maxim <tichkov_maxim@mail.ru>
2016-11-26 14:56:12 +0200

    implements Cart entity

--- src/main/java/com/epam/tm/shop/entity/Cart.java
+++ src/main/java/com/epam/tm/shop/entity/Cart.java
@@ -1,9 +1,24 @@
 package com.epam.tm.shop.entity;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Cart extends BaseEntity{
-    private List<Map<Product,Integer>> cart;
+    private Map<Product,Integer> cart;
+
+    public Cart() {
+        cart = new HashMap<>();
+    }
+
+    public boolean addProduct(Product product, Integer quantity){
+        return cart.put(product, quantity) != null;
+    }
+
+    public boolean removeProduct(Product product){
+        return cart.remove(product) != null;
+    }
+
+    public boolean changeQuantity(Product product, Integer quantity){
+        return cart.replace(product, quantity) != null;
+    }
 }
