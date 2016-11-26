6a7fc4a2c0ffe5118b33c994852613ad54299bcb
Tychkov Maxim <tichkov_maxim@mail.ru>
2016-11-26 16:18:17 +0200

    add Category entity

--- src/main/java/com/epam/tm/shop/entity/Category.java
+++ src/main/java/com/epam/tm/shop/entity/Category.java
@@ -1,5 +1,29 @@
 package com.epam.tm.shop.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Category extends BaseEntity{
 
+    private List<String> categories;
+    private int i;
+
+    public Category() {
+        categories = new ArrayList<>();
+        i = 1;
+    }
+
+    public String getCategory(){
+        return categories.get(0);
+    }
+
+    public String getSubCategory(){
+        if (categories.size() >= i) i = 1;
+        if (categories.size() == 1) i = 0;
+        return categories.get(i++);
+    }
+
+    public int getAmountSubCategory(){
+        return categories.size()-1;
+    }
 }
