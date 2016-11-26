a924143c41a5573d4943174d8a748d51fbccdc14
Tychkov Maxim <tichkov_maxim@mail.ru>
2016-11-26 15:41:40 +0200

    add Product entity

--- src/main/java/com/epam/tm/shop/entity/Product.java
+++ src/main/java/com/epam/tm/shop/entity/Product.java
@@ -7,5 +7,56 @@ public class Product extends BaseEntity{
     private String description;
     private Money price;
     private Category category;
+    private String imagePath;
 
+    public Product() {
+    }
+
+    public Product(String name, String description, Money price, Category category, String imagePath) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+        this.imagePath = imagePath;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+
+    public void setPrice(Money price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
 }
