2cdca368c200ddaa374629a151e2494c4e881350
Tychkov Maxim <tichkov_maxim@mail.ru>
2016-11-26 15:17:11 +0200

    add Order entity

--- src/main/java/com/epam/tm/shop/entity/Order.java
+++ src/main/java/com/epam/tm/shop/entity/Order.java
@@ -8,5 +8,56 @@ public class Order extends BaseEntity {
     private User user;
     private DateTime time;
     private Money total;
+    private OrderStatus status;
 
+    public Order() {
+    }
+
+    public Order(Cart cart, User user, DateTime time, Money total, OrderStatus status) {
+        this.cart = cart;
+        this.user = user;
+        this.time = time;
+        this.total = total;
+        this.status = status;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public DateTime getTime() {
+        return time;
+    }
+
+    public void setTime(DateTime time) {
+        this.time = time;
+    }
+
+    public Money getTotal() {
+        return total;
+    }
+
+    public void setTotal(Money total) {
+        this.total = total;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 }
