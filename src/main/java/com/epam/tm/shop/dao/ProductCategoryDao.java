98404ed0e79cb4339d36eaa14c365d8906e68312
Tychkov Maxim <tichkov_maxim@mail.ru>
2016-11-27 12:38:42 +0200

    impl JdbcProductCategoryDao del, save, update

--- src/main/java/com/epam/tm/shop/dao/ProductCategoryDao.java
+++ src/main/java/com/epam/tm/shop/dao/ProductCategoryDao.java
@@ -1,4 +1,7 @@
 package com.epam.tm.shop.dao;
 
+import java.sql.Connection;
+
 public interface ProductCategoryDao {
+    void setCon(Connection con);// FIXME: 22.11.2016
 }
--- src/main/java/com/epam/tm/shop/dao/jdbc/JdbcProductCategory.java
+++ src/main/java/com/epam/tm/shop/dao/jdbc/JdbcProductCategory.java
@@ -7,9 +7,15 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class JdbcProductCategory extends JdbcDao<ProductCategory> implements ProductCategoryDao{
+public class JdbcProductCategory extends JdbcDao<ProductCategory> implements ProductCategoryDao {
+
+    private static final String INSERT_QUERY = "INSERT INTO categories VALUES(DEFAULT,?,?)";
+    private static final String UPDATE_QUERY = "UPDATE categories SET name = ?, description = ? WHERE id = ?";
+    private static final String SELECT_QUERY = "SELECT * FROM categories WHERE products.id = ?";
+    private static final String DELETE_QUERY = "DELETE FROM categories WHERE id = ?";
 
     public JdbcProductCategory(Connection connection) {
         super(connection);
@@ -17,31 +23,65 @@ public class JdbcProductCategory extends JdbcDao<ProductCategory> implements Pro
 
     @Override
     protected List<ProductCategory> createEntityFromResultSet(ResultSet rs) throws SQLException, JdbcException {
-        return null;
+        List<ProductCategory> productCategories = new ArrayList<>();
+        try {
+            while (rs.next()) {
+                ProductCategory productCategory = new ProductCategory();
+                productCategory.setId(rs.getInt("id"));
+                productCategory.setName(rs.getString("name"));
+                productCategory.setDescription(rs.getString("description"));
+                productCategories.add(productCategory);
+            }
+        } catch (SQLException e) {
+            log.error("creating product category entity from result set was failed");
+            throw new JdbcException(e);
+        }
+
+        if (productCategories.size() == 0)
+            throw new JdbcException("no one product category was found");
+
+        return productCategories;
     }
+
 
     @Override
     protected void setPsFields(PreparedStatement ps, ProductCategory entity) throws JdbcException {
+        try {
+            ps.setString(1, entity.getName());
+            ps.setString(2, entity.getDescription());
 
+            Integer id = entity.getId();
+            if (id != null)
+                ps.setInt(3, entity.getId());
+
+        } catch (SQLException e) {
+            log.error("set product category entity to ps was failed");
+            throw new JdbcException(e);
+        }
     }
 
     @Override
     protected String getSelectQueryById() {
-        return null;
+        return SELECT_QUERY;
     }
 
     @Override
     protected String getUpdateQuery() {
-        return null;
+        return UPDATE_QUERY;
     }
 
     @Override
     protected String getInsertQuery() {
-        return null;
+        return INSERT_QUERY;
     }
 
     @Override
     protected String getDeleteQuery() {
-        return null;
+        return DELETE_QUERY;
+    }
+
+    // FIXME: 22.11.2016
+    public void setCon(Connection con) {
+        this.connection = con;
     }
 }
