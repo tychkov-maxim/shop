6d16f8fab6fb7f02b2599e37cf251c21612c426f
Tychkov Maxim <tichkov_maxim@mail.ru>
2017-01-07 15:14:37 +0200

    fix NotEmptyParameterValidator

--- src/main/java/com/epam/tm/shop/validator/NotEmptyParameterValidator.java
+++ src/main/java/com/epam/tm/shop/validator/NotEmptyParameterValidator.java
@@ -3,6 +3,6 @@ package com.epam.tm.shop.validator;
 public class NotEmptyParameterValidator extends ValidatorImpl {
     @Override
     public boolean isValid(String value) {
-        return ((value != null) && value.equals(""));
+        return ((value != null) && (!value.equals("")));
     }
 }
