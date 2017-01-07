fe7443245d332640843219131538e299ccfd9279
Tychkov Maxim <tichkov_maxim@mail.ru>
2017-01-07 15:04:45 +0200

    add NotEmptyParameterValidator

---
+++ src/main/java/com/epam/tm/shop/validator/NotEmptyParameterValidator.java
@@ -0,0 +1,8 @@
+package com.epam.tm.shop.validator;
+
+public class NotEmptyParameterValidator extends ValidatorImpl {
+    @Override
+    public boolean isValid(String value) {
+        return ((value != null) && value.equals(""));
+    }
+}
